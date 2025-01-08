5fd1f0cefabb3e5b96f2f519f1d48a6ad6ff774a
jeevanhd <jeevan.hd@kalvium.community>
2025-01-08 14:35:09 +0530

    fixed small errors

--- Backend/src/Routes/product.route.js
+++ Backend/src/Routes/product.route.js
@@ -1,15 +1,15 @@
 const multer = require("multer");
 const upload = multer({ dest: "temp-uploads/" });
 const express = require("express");
+const router = express.Router();
 
 const {
   createProductController,
   getProductDataController,
 } = require("../Controllers/product.controller.js");
-const router = express.Router();
 
 router.post(
-  "./create-product",
+  "/create-product",
   upload.array("files", 3),
   createProductController
 );
